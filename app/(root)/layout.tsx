e2c265307eab54d433d946a18462e7fa5b6c895e
MuhdAmin93 <mohammadidriis93@gmail.com>
2023-10-22 09:37:22 +0300

    implement theme provider

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <main className="background-light850_dark100"></main>;
+};
+
+export default Layout;
